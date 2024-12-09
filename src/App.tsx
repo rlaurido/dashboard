f29b3b688540d23fa150be320563d01a2aec8b2d
rlaurido <153690410+rlaurido@users.noreply.github.com>
2024-12-09 08:37:50 -0500

    cambios

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+import './App.css'
+import Grid from '@mui/material/Grid2' 
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Grid container spacing={5}>
+
+{/* Indicadores */}
+<Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 1</Grid>
+<Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 2</Grid>
+<Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 3</Grid>
+<Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 4</Grid>
+
+{/* Tabla */}
+<Grid size={{ xs: 12, xl: 8 }}>Elemento: Tabla</Grid>
+
+{/* Gráfico */}
+<Grid size={{ xs: 12, xl: 4 }}>Elemento: Gráfico 1</Grid>
+
+</Grid>
+    </>
+  )
+}
+
+export default App
